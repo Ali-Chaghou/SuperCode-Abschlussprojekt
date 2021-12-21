a31ed742bc10a01078a28db32b2018725d5f4e1c
Ali Chaghou <email@ali.supercode.gmail.com>
2021-12-21 19:52:42 +0100

    first commit

--- src/Components/DetailSite_Competitions.js
+++ src/Components/DetailSite_Competitions.js
@@ -1,7 +1,7 @@
 import "./DetailSite_Competitions.css";
 
 const  DetailSite_Competitions = () => {
-    return ( <h2>DetailSite_Competitions Component and CSS import</h2>  );
+    return ( <h2>DetailSite_Competition Component and CSS import</h2>  );
 }
  
 export default DetailSite_Competitions;
