ef66ce527d9acf4d05413d777b77dd147d61dd7b
Ali Chaghou <email@ali.supercode.gmail.com>
2021-12-22 20:47:56 +0100

    delete Detailsite Components and change Link Animation styling

--- src/Components/DetailSiteTeamHeader.js
+++
@@ -1,7 +0,0 @@
-import "./DetailSiteTeamHeader.css";
-
-const DetailSiteTeamHeader = () => {
-    return (  <h2>DetailSite_TeamHeader Component and CSS import</h2>  );
-}
- 
-export default DetailSiteTeamHeader;
