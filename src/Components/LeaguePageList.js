8503653c31e37412cb54f65bd2720f06b46fdb70
Ali Chaghou <email@ali.supercode.gmail.com>
2022-01-05 20:48:47 +0100

    delete classname="test"

--- src/Components/LeaguePageList.js
+++ src/Components/LeaguePageList.js
@@ -1,7 +1,22 @@
 import "./LeaguePageList";
 
-const LeaguePageList = () => {
-    return ( <h2>LeaguePage List Component and CSS import</h2> );
+//Link
+import { Link } from "react-router-dom";
+
+const LeaguePageList = (props) => {
+    console.log(props.leagues)
+    return ( 
+        <main>
+        {props.leagues.map((league) => (
+          <article key={league.strSport}>
+            <h2>{league.strLeagues}</h2>
+            <Link to={`/league/${league._id}`} id={league._id}>
+            {league.strLeague}
+            </Link>
+          </article>
+        ))}
+      </main>
+     );
 }
  
 export default LeaguePageList;
