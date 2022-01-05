09c2f62ca3c4bbeb55aa1ebcb92a1948c9675242
Ali Chaghou <email@ali.supercode.gmail.com>
2022-01-05 19:30:14 +0100

    Leaguelist for the Api Fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 
 //Routing
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
@@ -7,25 +8,93 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./Components/NavBar";
 
 
+
 //Pages
 import DetailSite from "./Pages/DetailSite";
 import Home from "./Pages/Home";
 import LeaguePage from "./Pages/LeaguePage";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        {/*Pages*/}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/league" element={<LeaguePage />} />
-          <Route path="/details" element={<DetailSite />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+
+class App extends React.Component {
+  constructor() {
+  super()
+  this.state = {
+  leagues: [],
+  };
+}
+
+
+  componentDidMount() {
+    let pathname = window.location.pathname;
+    let id = pathname.substring(pathname.lastIndexOf("/") + 1, pathname.length);
+
+
+    fetch(`https://www.thesportsdb.com/api/v1/json/2/all_leagues.php/${id}`)
+      .then((res) => res.json())
+      .then((res) => {
+        (console.log(res.leagues))
+        this.setState({leagues: res.leagues});
+  });
+  }
+
+//   Searchbar = () => {
+
+//     let searchInput = document.querySelector('#input-search').value.toUpperCase();
+//     let searching = this.state.leagues.filter((league) => league.strSport.toUpperCase().includes(searchInput));
+//     this.setState({leagues: searching});
+// }
+
+  render() {
+  return(
+        <BrowserRouter>
+        <div className="App">
+          
+          <Navbar />
+
+
+
+
+          <main>
+            {this.state.leagues.map((league) => (
+              <article key={league}>
+                <h2 onClick={clicked}>{league.strLeague}</h2>
+                
+              </article>
+            ))}
+          </main>
+
+
+
+
+
+          {/*Pages*/}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/league" element={<LeaguePage />} />
+            <Route path="/details" element={<DetailSite />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+  )
+  };
+
 }
 
 export default App;
+
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className="App">
+        
+//         <Navbar />
+//         {/*Pages*/}
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/league" element={<LeaguePage />} />
+//           <Route path="/details" element={<DetailSite />} />
+//         </Routes>
+//       </div>
+//     </BrowserRouter>
+//   );
